ecaa8cf28ff0d8ffd3c728de1fb930d5bd2c0c45
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2022-11-18 17:26:20 +0000

    adding routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import { useState } from 'react'
+import "bootstrap/dist/css/bootstrap.min.css"
 import reactLogo from './assets/react.svg'
 import './App.css'
+import { Navigate, Route, Routes } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <h1>Hi</h1>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/new" element={<h1>New</h1>} />
+        <Route path="*" element={<Navigate to="/"/>} />
+      </Routes>
+    
     </div>
   )
 }
