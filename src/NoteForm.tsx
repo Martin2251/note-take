c9c589fe03e1949f16a5a721d083fafed6c914a2
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2022-11-19 09:44:10 +0000

    refs

--- src/NoteForm.tsx
+++ src/NoteForm.tsx
@@ -1,24 +1,39 @@
-import { Col, Form, Row, Stack } from "react-bootstrap"
+import { useRef } from "react"
+import { Button, Col, Form, Row, Stack } from "react-bootstrap"
+import { Link } from "react-router-dom"
+import CreateablerReactSelect from "react-select/creatable"
 
 const NoteForm = () => {
+const titleRef = useRef<HTMLInputElement>(null)
+const markdownRef = useRef<HTMLTextAreaElement>(null)
+
 return<Form>
 <Stack gap={4}>
    <Row>
        <Col>
        <Form.Group controlId="title">
            <Form.Label>Title</Form.Label>
-           <Form.Control required />
+           <Form.Control ref={titleRef}required />
 
        </Form.Group>
        </Col>
        <Col>
        <Form.Group controlId="tags">
            <Form.Label>Tags</Form.Label>
-           <Form.Control required />
-
+           <CreateablerReactSelect isMulti/>
        </Form.Group>
        </Col>
    </Row>
+   <Form.Group controlId="markdown">
+           <Form.Label>Body</Form.Label>
+           <Form.Control required as = "textarea" ref={markdownRef}rows={15}/>
+       </Form.Group>
+       <Stack direction="horizontal" gap={2}className="justify-content-end">
+           <Button type="submit" variant="primary">Save</Button>
+           <Link to= "..">
+           <Button type="button" variant="outline-secondary">Cancel</Button>
+           </Link>
+       </Stack>
 </Stack>
 
 </Form>
