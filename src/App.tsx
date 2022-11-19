09211e6d7ebfcb496c067b09501029a2e9f0cdbb
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2022-11-19 11:03:01 +0000

    update

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Navigate, Route, Routes } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import NewNote from './NewNote'
 import useLocalStorage from './useLocalStorage'
+import {v4 as uuidV4} from "uuid"
 
 export type Note  = {
   id: string
@@ -22,7 +23,7 @@ export type RawNoteData = {
  export type NoteData = {
   title:string,
   markdown:string,
-  tags: Tag[]
+  tags: Tag[],
 }
 
  export type Tag = {
@@ -40,18 +41,20 @@ function App() {
     })
   }, [notes, tags])
 
-  const onCreateNote = ({tags,...data}: NoteData) => {
-    setNotes(prevNotes =>{
-      return [...prevNotes, {...data, id: uuidV4()}]
+  function onCreateNote({ tags, ...data }: NoteData) {
+    setNotes(prevNotes => {
+      return [
+        ...prevNotes,
+        { ...data, id: uuidV4(), tagIds: tags.map(tag => tag.id) },
+      ]
     })
-
   }
 
   return (
     <Container className='my-4'>
       <Routes>
         <Route path="/" element={<h1>Home</h1>} />
-        <Route path="/new" element={<NewNote  />} />
+        <Route path="/new" element={<NewNote onSubmit={onCreateNote} />} />
         <Route path="/:id">
           <Route index element={<h1>Show</h1>} />
           <Route  path="edit" element={<h1>Edit</h1>} />
