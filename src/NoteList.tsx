d83f11e6cb92aa4af72b76a2e81f04b6a7d1b423
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2022-11-20 10:44:36 +0000

    notelist

--- src/NoteList.tsx
+++ src/NoteList.tsx
@@ -1,7 +1,11 @@
+import { useState } from "react"
 import { Button, Col, Row, Stack, Form } from "react-bootstrap"
 import { Link } from "react-router-dom"
+import ReactSelect from "react-select"
+import { Tag } from "./App"
 
-const NoteList = () => {
+const NoteList = ({availableTags,}) => {
+    const [selectedTags, setSelectedTages] = useState<Tag[]>([])
 return(
    <>
    <Row>
@@ -21,8 +25,29 @@ return(
   <Form>
     <Row className="mb-4">
         <Col>
-        <Form.Group controlId="title"></Form.Group>
+        <Form.Group controlId="title">
+            <Form.Label>Title</Form.Label>
+            <Form.Control type="text"  />
+        </Form.Group>
         
+        </Col>
+        <Col>
+        <Form.Group controlId="tags">
+           <Form.Label>Tags</Form.Label>
+           <ReactSelect 
+           value ={selectedTags.map(tag =>{
+               return {label: tag.label, value:tag.id}
+           })}
+           options={availableTags.map(tag => {
+               return {label: tag.label, value:tag.id}
+           })}
+           onChange={tags => {
+               setSelectedTages(tags.map(tag => {
+                   return {label: tag.label, id:tag.value}
+               }))
+           }}
+           isMulti/>
+       </Form.Group>
         </Col>
     </Row>
   </Form>
